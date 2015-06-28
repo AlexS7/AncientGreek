2c025c4d8145794afc55e9b00e6f5f59665fd4a7
alex <alexandrosrotsides@gmail.com>
2015-06-28 13:15:18 +0100

    added citation and description files download

--- grails-app/controllers/alrosh7/CitationFileController.groovy
+++ grails-app/controllers/alrosh7/CitationFileController.groovy
@@ -5,4 +5,14 @@ class CitationFileController {
     def scaffold = CitationFile
     
     def index() { }
+
+    def download(){
+        def citationFile = CitationFile.get(params.id);
+        def file = new File(citationFile.pathToFile)
+
+        response.setContentType("application/octet-stream")
+        response.setHeader("Content-disposition", "attachment;filename=${file.getName()}")
+
+        response.outputStream << file.newInputStream()
+    }
 }
--- grails-app/controllers/alrosh7/DescriptionFileController.groovy
+++ grails-app/controllers/alrosh7/DescriptionFileController.groovy
@@ -5,4 +5,14 @@ class DescriptionFileController {
     def scaffold = DescriptionFile
 
     def index() { }
+
+    def download(){
+        def descriptionFile = DescriptionFile.get(params.id);
+        def file = new File(descriptionFile.pathToFile)
+
+        response.setContentType("application/octet-stream")
+        response.setHeader("Content-disposition", "attachment;filename=${file.getName()}")
+
+        response.outputStream << file.newInputStream()
+    }
 }
